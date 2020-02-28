43c4d3b1bda14424050ec4491fe9ce7fc6fb318c
Deijah Price <deijahwilson1@gmail.com>
2020-02-28 11:53:01 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -9,18 +9,21 @@ def find_item_by_name_in_collection(name, collection)
   end
 end
 
-def consolidate_cart(cart)
-  con_hash = {}
+def consolidate_cart(cart:[])
+  # code here
+  my_hash = {}
+
   cart.each do |item|
-    item.each do |name, attribute|
-      if con_hash.has_key?(name)
-        con_hash[name][:count] += 1        
-      else 
-        con_hash = con_hash.merge({name => attribute.merge({count: 1})})
+    item.each do |itemname, data|
+      if my_hash[itemname] == nil
+        my_hash[itemname] = data
+        my_hash[itemname][:count] = 1 
+      else
+        my_hash[itemname][:count] += 1
       end
     end
   end
-  return con_hash
+  my_hash
 end	
 
 def apply_coupons(cart, coupons)
