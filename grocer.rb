a790ec77bcae6ae7c9d8b728999dfb7a931e6778
Deijah Price <deijahwilson1@gmail.com>
2020-02-28 11:25:11 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -2,7 +2,7 @@ def find_item_by_name_in_collection(name, collection)
   counter=0 
   
   while counter < collection.length do 
-   if collection|counter|
+   if collection[counter][:name]
   end
 end
 
