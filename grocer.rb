52f4fcab6112ead4dda60ba91def70bd2170df29
Deijah Price <deijahwilson1@gmail.com>
2020-02-28 11:34:22 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -14,8 +14,8 @@ def consolidate_cart(cart)
   counter = 0 
   
   while counter< cart.length do 
-    new_cart_item= :count
-    cart.push
+    new_cart_item= find_item_by_name_in_collection()
+    
     counter +=1
 end
 
