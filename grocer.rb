3b559998a6afd5b7fc41448bc024078aeb3ed987
Deijah Price <deijahwilson1@gmail.com>
2020-02-28 11:45:04 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -19,7 +19,7 @@ def consolidate_cart(cart)
       new_cart_item[:count] += 1 
     else
       new_cart_item= {
-       :item=> cart[counter][:name],
+       :item=> cart[counter][:item],
        :price=> cart[counter][:price],
        :clearance=> cart[counter][:clearance],
        :count=> 1
