affaf35a8125b6ab8067b430694339f2c94cb6ad
Deijah Price <deijahwilson1@gmail.com>
2020-02-28 12:24:28 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -10,26 +10,18 @@ def find_item_by_name_in_collection(name, collection)
 end
 
 def consolidate_cart(cart)
-  new_cart=[]
-  counter = 0 
-  
-  while counter< cart.length 
-    new_cart_item= find_item_by_name_in_collection(cart[counter][:item], new_cart)
-    if new_cart_item !=nil 
-      new_cart_item[:count] += 1 
+   consCart = {}
+  cart.map { |groc|
+    grocItem = groc.keys[0]
+    if consCart[grocItem]
+      consCart[grocItem][:count] += 1
     else
-      new_cart_item= {
-       :item => cart[counter][:item],
-       :price => cart[counter][:price],
-       :clearance => cart[counter][:clearance],
-       :count => 1
-      }
-    new_cart << new_cart_item
-     end
-    counter += 1
-end
-new_cart
-end
+      consCart[grocItem] = groc[grocItem]
+      consCart[grocItem][:count] = 1
+    end
+  }
+  consCart
+end	
 
 def apply_coupons(cart, coupons)
   counter = 0 
